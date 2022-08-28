3a5895523686b39a3a5ef25cfb4d60a51849ffa0
aureljoss <josserand.n.aurelie@gmail.com>
2022-08-28 18:26:07 -0400

    Fixes scroll indicator positioning and style.

--- scripts/app.js
+++ scripts/app.js
@@ -164,7 +164,7 @@ window.onscroll = function() {myFunction()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  var width = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / width) * 100;
+  document.getElementById("myBar").style.height = scrolled + "%";
 }
