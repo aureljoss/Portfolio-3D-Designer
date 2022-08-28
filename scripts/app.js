b36ab29620f2cbae144bd025f22f52a38edc0d88
aureljoss <josserand.n.aurelie@gmail.com>
2022-08-28 18:08:29 -0400

    Adds draft scroll indicator. Fixes broken img link.

--- scripts/app.js
+++ scripts/app.js
@@ -156,3 +156,15 @@ function enterHello(){
 function leaveHello(){
   social.style.transform="scale(1)";
 }
+
+
+// ----- Scroll Indicator ----- //
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
