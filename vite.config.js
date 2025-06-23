5593b93f0c742a756d892391cc24e95232342ddd
Aurelie <josserand.n.aurelie@gmail.com>
2025-06-23 11:56:08 -0400

    Moves html files to public folder and updates relative paths in index.html only

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 export default defineConfig({
   plugins: [react()],
   base: "/",
@@ -9,7 +10,12 @@ export default defineConfig({
     assetsDir: "assets",
     sourcemap: true,
   },
-  rollupOptions: {
-    input: ['index.html', 'about.html', 'VT.html', 'KSU.html']
-  },
+//   rollupOptions: {
+//     input: {
+//         index: "./index.html",
+//         about: "./about.html",
+//         vt: "./VT.html",
+//         ksu: "./KSU.html",
+//     },
+//   },
 });
