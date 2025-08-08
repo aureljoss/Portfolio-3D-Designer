1dddc4c7c17ad759d8b8aaa4a3be0ba4b1f973bf
Aurelie <josserand.n.aurelie@gmail.com>
2025-08-08 18:38:10 -0400

    Fixes "my project" button and adds modile responsive attributes to index.html

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -49,7 +49,7 @@ export function Experience() {
           {/* <AccumulativeShadows frames={100} color={"#3d1b65"} colorBlend={5} toneMapped={true} alphaTest={0.9} opacity={1} scale={30} position={[0, -10, 0]}>
             <RandomizedLight amount={4} radius={10} ambient={0.5} intensity={1} position={[0, 10, -10]} size={15} mapSize={1024} bias={0.0001} />
           </AccumulativeShadows> */}
-        <Center  position={[0, 0.5, -0.5]}>
+        <Center  position={[0, 0.5, -1]}>
           <mesh geometry={nodes.baked.geometry}>
             <meshBasicMaterial map={bakedTexture} side={DoubleSide}/>
           </mesh>
--- src/index.jsx
+++ src/index.jsx
@@ -4,35 +4,64 @@ import ReactDOM from "react-dom/client";
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-import { Html, useProgress} from '@react-three/drei' // for loading progress
-
+import { Html } from "@react-three/drei"; // for loading progress
 
 function Loader() {
-  return <Html center><div id="loader-threejs">Something fun is loading...</div></Html>
+  return (
+    <Html center>
+      <div id="loader-threejs">Something fun is loading...</div>
+    </Html>
+  );
 }
 
 function Overlay() {
+  const handlePortfolioClick = (e) => {
+    e.preventDefault();
+    const portfolioSection = document.getElementById(
+      "portfolio-projects-section"
+    );
+    if (portfolioSection) {
+      portfolioSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div id="intro-threejs">
       <h3>&lt; 3D Designer & Creative Developer /&gt;</h3>
       <p>
-        My name is Aurélie (pronounced oh-reh-lee). <br/>
-        I bring ideas to life through immersive 3D design and innovative digital experiences.
+        My name is Aurélie (pronounced oh-reh-lee). <br />I bring ideas to life
+        through immersive 3D design and innovative digital experiences.
       </p>
-      <ul id="contact-threejs">
+      <ul id="projects-threejs">
         <li>
-          <a href="http://aureliejosserand.com#portfolio-projects-section" className="button-threejs">
+          <a
+            href="#portfolio-projects-section"
+            className="button-threejs"
+            onClick={handlePortfolioClick}
+          >
             My Projects
           </a>
         </li>
       </ul>
       <div id="scroll">
-            <svg version="1.1" id="scrollArrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="53px" height="20px" viewBox="0 0 53 20" enable-background="new 0 0 53 20" xml:space="preserve">
-                <g>
-                    <polygon points="26.5,17.688 9.114,3.779 10.303,2.312 26.5,15.269 42.697,2.313 43.886,3.779 	"></polygon>
-                </g>
-            </svg>
-        </div>
+        <svg
+          version="1.1"
+          id="scrollArrow"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          width="53px"
+          height="20px"
+          viewBox="0 0 53 20"
+          enable-background="new 0 0 53 20"
+          xml:space="preserve"
+        >
+          <g>
+            <polygon points="26.5,17.688 9.114,3.779 10.303,2.312 26.5,15.269 42.697,2.313 43.886,3.779 	"></polygon>
+          </g>
+        </svg>
+      </div>
     </div>
   );
 }
